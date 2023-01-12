96d2f83db406997f3da199f1aed527a2942457fb
DanaHa-24 <dana24997@gmail.com>
2023-01-13 00:44:29 +0200

    ver1.4

--- src/components/ToDoInput/index.js
+++ src/components/ToDoInput/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function TodoInput({ newTodoInput, handleAddTodo }) {
   return (
-    <div todo-box>
+    <div class="todo-container">
         <input className="todo-input" ref={newTodoInput} type="text" placeholder="Create a new todo..." />
         <button className="todo-submit" onClick={handleAddTodo}>+</button>
     </div>
